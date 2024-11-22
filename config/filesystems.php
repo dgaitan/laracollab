b3a8c5496c418c43387feedf71f605ba30cfcfef
David Gaitan <jdavid.gaitan@gmail.com>
2024-11-22 10:43:31 -0600

    default flysystem

--- config/filesystems.php
+++ config/filesystems.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('FILESYSTEM_DISK', 'local'),
+    'default' => env('FILESYSTEM_DISK', 's3'),
 
     /*
     |--------------------------------------------------------------------------
@@ -39,14 +39,14 @@ return [
         'invoices' => [
             'driver' => 'local',
             'root' => storage_path('app/invoices'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'private',
         ],
 
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
             'throw' => false,
         ],
